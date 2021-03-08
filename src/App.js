3b093808c64d883b65b11720277aa90747f3411b
jonasa64 <hvidovrefifa@gmail.com>
2021-03-08 22:19:21 +0100

    renamed file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import InputCityName from "./components/WeatherCity";
+import InputCityName from "./components/WeatherCityInput";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
