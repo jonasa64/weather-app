9072543747c35ac8e603fe6a34e0115e579ce0f7
jonasa64 <hvidovrefifa@gmail.com>
2021-03-08 20:32:30 +0100

    added icon for temperatur and function to format temp

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -3,10 +3,12 @@ import useIcon from "../hooks/useIcon";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'weather-icons/css/weather-icons.css'
 
-const DisplayWeather = ({data}) => {
-    const {weather, main, wind, sys, name} = data[0];
+const DisplayWeather = ({data, setCity, setSubmitted}) => {
+    const {weather, main, name} = data[0];
     const rangeId = weather.map(item => item.id)[0];
     const {icon} = useIcon(rangeId);
+
+    const formatTemp = temp => Math.floor(temp);
 
     return (
         <div className="card mt-4">
@@ -22,9 +24,12 @@ const DisplayWeather = ({data}) => {
                     </div>
                 ))}
 
-                <h3 className="py-2 text-center text-secondary">Current temperatur {main.temp} deg</h3>
-                <p className="text-center"><span className="px-4"> Min temperatur {main.temp_min} deg</span> <span
-                    className="px-4">Max temperatur {main.temp_max} deg</span></p>
+                <h3 className="py-2 text-center text-secondary">Current temperatur { formatTemp(main.temp)} <i
+                    className="wi wi-celsius"/></h3>
+                <p className="text-center"><span className="px-4"> Min temperatur {formatTemp(main.temp_min)} <i
+                    className="wi wi-celsius"/> </span> <span
+                    className="px-4">Max temperatur {formatTemp(main.temp_max)} <i className="wi wi-celsius"/></span>
+                </p>
             </div>
         </div>
     )
